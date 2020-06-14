0b8ad1dad833b2ae7b638f5556d9192ca0175ebc
Will Ballard <wballard@mailframe.net>
2020-06-14 07:32:11 -0500

    updated docs for autocrud to include files by reference

--- packages/engine/tests/sqlmodules-autocrud.test.ts
+++ packages/engine/tests/sqlmodules-autocrud.test.ts
@@ -6,6 +6,7 @@ import { migrate } from "../src/migrations";
 import rmfr from "rmfr";
 import { createServer } from "..";
 import http from "http";
+import { example } from "./examples/orders_items";
 
 describe("sqlmodules provide autocrud", () => {
   let rootContext: InternalContext;
@@ -162,75 +163,10 @@ describe("sqlmodules provide autocrud", () => {
       await cycle(nodeHttpClient);
     });
   });
-  it("works like the documentation", async () => {
-    const client = engine;
-    // little shortcut to type less...
-    const db = client.databases.default.autocrud;
-
-    // make a single item -- comes back with an object
-    const item_key = await db.items.create({ description: "Paper" });
-
-    // and array valued, make a few records in one shot...
-    const more_item_keys = await db.items.create(
-      { description: "Can" },
-      { description: "Loaf" }
-    );
-
-    // no need to pass in the key, it is an auto increment, but you
-    // sure will need it to put in order items, so let's capture the created key
-    // whih EmbraceSQL thought of for you
-    const order_key = await db.orders.create({ name: "Sample" });
-
-    // now join items and orders with one of each
-    // again notice we don't need to mention the autoincrement
-    await db.order_items.create(
-      [item_key, ...more_item_keys].map((item_key) => ({
-        order_id: order_key.order_id,
-        item_id: item_key.item_id,
-        quantity: 1,
-      }))
-    );
-
-    // You can use that created key to 'read back' a record, kinda handy
-    // oh -- and it pulls back the whole associated referential graph
-    // your reward for embracing sql and referential integrity is not needing
-    // to query to get child records like order_items or lookup data like items
-    const my_order = await db.orders.read(order_key);
-
-    // notice three tables are joined automatically and you can get the description
-    // EmbraceSQL inside created a query batch to pull in all this data
-    // -- in one trip to the database, so it's not a chatterbox like ORMs
-    /*
-    console.log(
-      my_order.name,
-      my_order.order_items,
-      my_order.order_items[0].description
-    );
-    */
-
-    // And sometimes you really do need the whole table
-    // This is even simpler, just don't pass any parameters!
-    const all_orders = await db.orders.read();
-    console.log(all_orders);
-
-    // hmm -- I really want two of that... let's update, passing in all the order items
-    // and using nice object spread with one property of overwrite
-    await db.order_items.update({
-      ...my_order.order_items,
-      quantity: 2,
+  describe("works like the documentation", async () => {
+    it("in process", async () => {
+      await example(engine);
     });
-
-    // nope -- I don't want it at all
-    // delete the whole thing down the referentia graph
-    await db.orders.delete(order_key);
-
-    // but the items aren't deleted -- they are lookup data!
-    // EmbraceSQL is smart enough to only delete children
-    console.log(await db.items.read());
-
-    // clean up the items
-    await db.items.delete(await db.items.read());
-    console.log(await db.items.read());
   });
   it("works like the documentation over http", async () => {});
 });
